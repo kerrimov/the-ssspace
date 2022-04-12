f343444fd7976ad91964f5a0b29e81c6940552e7
Volodymyr Klochan <vovkis13@gmail.com>
2022-04-12 20:47:39 +0300

    change classes name pattern in scss files, change default export to named etc.

--- src/components/LaunchDetails/index.ts
+++ src/components/LaunchDetails/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export { LaunchDetails } from './LaunchDetails';
-=======
-export { default } from './LaunchDetails';
->>>>>>> b949c5a (add LaunchDetails and CountdownTimer components, nui libs)
