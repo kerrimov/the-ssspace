3ad0d7f51f6491fc6e400478616a1488ba9d7187
PietyE <kvasovdim17@gmail.com>
2022-04-14 21:31:00 +0300

    added theme and wrapper into it, added packages which were depended for mui, added font for theme in index.scss, deleted yarn lock from .gitigrone

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { Routes } from './components/Routes';
 import { Header } from './components/Header';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme/theme';
 
 export const App = () => {
   const routes = useRoutes(Routes);
   return (
     <>
-      <Header />
-      {routes}
+      <ThemeProvider theme={theme}>
+        <Header />
+        {routes}
+      </ThemeProvider>
     </>
   );
 };
