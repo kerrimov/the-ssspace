6c40383da0167c4eee6412378fca9a271bd292d7
Aleksandra Shevchenko <aleksandra05sh@gmail.com>
2022-04-15 16:36:51 +0300

    header updated urls

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,22 +4,16 @@ import { AppBar, Box, Toolbar, Container, Button } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import {
-  homeUrl,
-  eventsUrl,
-  agenciesUrl,
-  astronautsUrl,
-  aboutUrl,
-} from '../../routes/constants';
+import { RoutesPath } from '../Routes/routesPath';
 import { instagramUrl, linkedInUrl, gitHubUrl } from './constants';
 import './Header.scss';
 
 const pages = [
-  { name: 'Home', url: homeUrl },
-  { name: 'Events', url: eventsUrl },
-  { name: 'Agencies', url: agenciesUrl },
-  { name: 'Astronauts', url: astronautsUrl },
-  { name: 'About', url: aboutUrl },
+  { name: 'Home', url: RoutesPath.HOME },
+  { name: 'Events', url: RoutesPath.EVENTS },
+  { name: 'Agencies', url: RoutesPath.AGENCIES },
+  { name: 'Astronauts', url: RoutesPath.ASTRONAUTS },
+  { name: 'About', url: RoutesPath.ABOUT },
 ];
 
 const socialNetworks = [
@@ -33,7 +27,7 @@ export const Header = () => {
     <AppBar position="fixed">
       <Container maxWidth="xl">
         <Toolbar>
-          <Box className="logo" component={Link} to={homeUrl}>
+          <Box className="logo" component={Link} to={RoutesPath.HOME}>
             <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
           </Box>
           <Box className="navPages" component="nav">
