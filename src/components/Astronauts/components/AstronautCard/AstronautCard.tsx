22dde62c75d0a27d7aad7bdb863e7b0eed8e1295
topsart <64708421+topsart@users.noreply.github.com>
2022-04-17 17:31:51 +0300

    add buttonIcon

--- src/components/Astronauts/components/AstronautCard/AstronautCard.tsx
+++ src/components/Astronauts/components/AstronautCard/AstronautCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  Container,
   Card,
   CardActions,
   CardContent,
@@ -9,55 +8,50 @@ import {
   Button,
   Typography,
 } from '@mui/material';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import './AstronautCard.scss';
 
 export const AstronautCard = () => {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <Typography
-        className="astronauts_header"
-        gutterBottom
-        variant="h2"
-        component="div"
-        sx={{ color: 'primary.dark' }}
-      >
-        Astronauts
-      </Typography>
-      <Card sx={{ maxWidth: 268 }} className="astronautCard">
-        <CardMedia
-          component="img"
-          height="240"
-          image="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/astronaut_images/julie_payette_image_20220122091651.jpeg"
-          alt="astronaut"
-        />
-        <CardContent className="astronautCardContent">
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{ color: 'primary.contrastText' }}
-          >
-            Julie Payette
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Canadian Space Agency
-          </Typography>
-        </CardContent>
-        <CardActions className="astronautCardActions">
-          <Button
-            variant="contained"
-            size="medium"
-            sx={{ color: 'primary.contrastText' }}
-            onClick={() => {
-              navigate('/astronauts/details');
-            }}
-          >
-            More info
-          </Button>
-        </CardActions>
-      </Card>
-    </Container>
+    <Card sx={{ maxWidth: 268 }} className="astronautCard">
+      <CardMedia
+        component="img"
+        height="240"
+        image="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/astronaut_images/julie_payette_image_20220122091651.jpeg"
+        alt="astronaut"
+      />
+      <CardContent className="astronautCardContent">
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ color: 'primary.contrastText' }}
+        >
+          Julie Payette
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Canadian Space Agency
+        </Typography>
+      </CardContent>
+      <CardActions className="astronautCardActions">
+        <Button
+          className="astronautCardActions_button"
+          variant="contained"
+          size="medium"
+          sx={{ color: 'black' }}
+          onClick={() => {
+            navigate('/astronauts/details');
+          }}
+        >
+          <InfoOutlinedIcon
+            fontSize="small"
+            className="astronautCardActions_buttonIcon"
+          />
+          Info
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
