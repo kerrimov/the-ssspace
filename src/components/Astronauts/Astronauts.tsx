b1fd2942e1397dded260625602af695aa8d87ab9
topsart <64708421+topsart@users.noreply.github.com>
2022-04-17 17:31:51 +0300

    add buttonIcon

--- src/components/Astronauts/Astronauts.tsx
+++ src/components/Astronauts/Astronauts.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container, Typography } from '@mui/material';
 import { AstronautCard } from './components/AstronautCard';
 
 export const Astronauts = () => {
   return (
-    <>
+    <Container>
+      <Typography
+        className="astronauts_header"
+        gutterBottom
+        variant="h2"
+        component="div"
+        sx={{ color: 'primary.dark' }}
+      >
+        Astronauts
+      </Typography>
       <AstronautCard />
-    </>
+    </Container>
   );
 };
