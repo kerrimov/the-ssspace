feaebc4a329db69891bee179e5081eb06e65926c
Volodymyr Klochan <vovkis13@gmail.com>
2022-04-17 22:39:49 +0300

    restore index.tsx and prettierrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import { App } from './App';
 
@@ -8,6 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
