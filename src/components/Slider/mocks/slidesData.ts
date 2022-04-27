33e021c5269be2f3de3396166f2dc6ba7aeaba18
PietyE <kvasovdim17@gmail.com>
2022-04-27 20:03:51 +0300

    added shared timer component into slider card

--- src/components/Slider/mocks/slidesData.ts
+++ src/components/Slider/mocks/slidesData.ts
@@ -1,9 +1,9 @@
-import type { Slides } from '../types/sliderTypes';
+import type { Slides } from '../types/SliderTypes';
 
 export const slides: Array<Slides> = [
   {
     name: 'Upcoming',
-    date: '2022-04-13T19:28:34Z',
+    date: '2022-05-01T19:28:34Z',
     imageURL:
       'https://upload.wikimedia.org/wikipedia/commons/d/d1/Falcon_Heavy_Demo_Mission_%2839337245145%29.jpg',
   },
