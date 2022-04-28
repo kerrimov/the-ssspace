83564484df58dd47773bc1def18114b8c9b4e379
Aleksandra Shevchenko <aleksandra05sh@gmail.com>
2022-04-28 20:17:20 +0300

    rename Agencies in AgenciesPage

---
+++ src/components/AgenciesPage/AgenciesPage.tsx
@@ -0,0 +1,13 @@
+import { Container, Grid, Paper } from '@material-ui/core';
+import React from 'react';
+
+export const AgenciesPage = () => (
+  <>
+    <h1>Agencies</h1>
+    <Paper>
+      <Container>
+        <Grid container></Grid>
+      </Container>
+    </Paper>
+  </>
+);
