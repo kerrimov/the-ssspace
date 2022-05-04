84b2b4fcf28800f4552d344c617b76222eac9a31
topsart <alexvigr@gmail.com>
2022-05-04 15:35:43 +0300

    change BrowserRouter to HashRouter in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.scss';
 
@@ -9,8 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
