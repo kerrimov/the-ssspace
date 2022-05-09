ef111bce390c151dca7d1d04746599ccf2f08726
Volodymyr Klochan <vovkis13@gmail.com>
2022-05-09 09:50:19 +0300

    add action creators and actions enum

--- src/components/LaunchFilter/actions/actionCreators.ts
+++ src/components/LaunchFilter/actions/actionCreators.ts
@@ -1,10 +1,10 @@
 import { AxiosError } from 'axios';
 import type { Launch } from '../../../shared/api/types/Launch';
 import { LaunchFilters } from '../constants/filerConstants';
-import { LAUNCH_ERROR, LAUNCH_REQUEST, LAUNCH_SUCCESS } from './actionTypes';
+import { LaunchActions } from './actionTypes';
 
 export const requesAction = (filterValue: LaunchFilters) => ({
-  type: `${filterValue}_${LAUNCH_REQUEST}`,
+  type: `${filterValue}_${LaunchActions.LAUNCH_REQUEST}`,
   payload: { filterValue },
 });
 
@@ -12,11 +12,11 @@ export const successAction = (
   filterValue: LaunchFilters,
   launches: Launch[],
 ) => ({
-  type: `${filterValue}_${LAUNCH_SUCCESS}`,
+  type: `${filterValue}_${LaunchActions.LAUNCH_SUCCESS}`,
   payload: { filterValue: filterValue, [filterValue]: launches },
 });
 
 export const errorAction = (filterValue: LaunchFilters, error: AxiosError) => ({
-  type: `${filterValue}_${LAUNCH_ERROR}`,
+  type: `${filterValue}_${LaunchActions.LAUNCH_ERROR}`,
   payload: { filterValue: filterValue, error: error as AxiosError },
 });
