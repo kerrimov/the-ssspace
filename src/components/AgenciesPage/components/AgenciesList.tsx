30f9002a4f93a0f102fe621046eb9aa3827c3ffb
Aleksandra Shevchenko <aleksandra05sh@gmail.com>
2022-05-11 00:54:44 +0300

    call to api agencies page are made

--- src/components/AgenciesPage/components/AgenciesList.tsx
+++ src/components/AgenciesPage/components/AgenciesList.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
 import { AgenciesItem } from './components';
-import { agenciesData } from '../mocks/agenciesData';
+import { fetchAgencies } from '../services/fetchAgencies';
+import { Agency } from '../types/Agencies';
 
-export const AgenciesList = () => (
-  <Grid container>
-    {agenciesData.map(agency => (
-      <Grid item key={agency.id} xs={12} sm={6} md={4} lg={3}>
-        <AgenciesItem agency={agency} />
-      </Grid>
-    ))}
-  </Grid>
-);
+export const AgenciesList = () => {
+  const [data, setData] = useState<Agency[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetchAgencies();
+      setData(response);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data.length) return null;
+
+  return (
+    <Grid container>
+      {data.map(agency => (
+        <Grid item key={agency.id} xs={12} sm={6} md={4} lg={3}>
+          <AgenciesItem agency={agency} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
