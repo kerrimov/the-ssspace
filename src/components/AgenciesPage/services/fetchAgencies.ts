b3694900051c0511228e93827a8b3608e10fd759
Aleksandra Shevchenko <aleksandra05sh@gmail.com>
2022-05-12 23:03:31 +0300

    removed try catch in fetchAgencies

--- src/components/AgenciesPage/services/fetchAgencies.ts
+++ src/components/AgenciesPage/services/fetchAgencies.ts
@@ -1,11 +1,8 @@
 import { Endpoints } from '../../../shared/api/constants/endpoints';
 import { axiosInstance } from '../../../shared/api/services/axiosInstance';
+import { Agency } from '../types/Agencies';
 
-export const fetchAgencies = async () => {
-  try {
-    const { data } = await axiosInstance.get(Endpoints.AGENCIES_ENDPOINT);
-    return data.results;
-  } catch (error) {
-    console.error(error);
-  }
+export const fetchAgencies = async (): Promise<Agency[]> => {
+  const { data } = await axiosInstance.get(Endpoints.AGENCIES_ENDPOINT);
+  return data.results;
 };
