cd990be0e0c07f170203aab1c6a423491aed0cdb
topsart <alexvigr@gmail.com>
2022-05-18 19:19:28 +0300

    fix astronauts endpoint and loader issue

--- src/components/AstronautsPage/components/AstronautsList/AstronautsList.tsx
+++ src/components/AstronautsPage/components/AstronautsList/AstronautsList.tsx
@@ -7,30 +7,29 @@ import type { Astronauts } from '../../types/Astronauts';
 export const AstronautsList = () => {
   const { astronauts, isLoading } = selectAstronauts();
 
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  } else if (!astronauts.length) {
+  if (isLoading) return <p>Loading....</p>;
+
+  if (!astronauts.length)
     return (
       <h2 className="astronauts-list-empty">
         No astronauts for now. Try later.
       </h2>
     );
-  } else {
-    return (
-      <Grid container>
-        {astronauts.map(
-          ({ name, agency, profile_image }: Astronauts, index: number) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-              <AstronautsItem
-                key={index}
-                name={name}
-                agency={agency}
-                profile_image={profile_image}
-              />
-            </Grid>
-          ),
-        )}
-      </Grid>
-    );
-  }
+
+  return (
+    <Grid container>
+      {astronauts.map(
+        ({ name, agency, profile_image }: Astronauts, index: number) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+            <AstronautsItem
+              key={index}
+              name={name}
+              agency={agency}
+              profile_image={profile_image}
+            />
+          </Grid>
+        ),
+      )}
+    </Grid>
+  );
 };
