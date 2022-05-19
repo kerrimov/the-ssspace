43ecf69acdd635202b1af30a251079224d99468d
Aleksandra Shevchenko <aleksandra05sh@gmail.com>
2022-05-19 23:15:49 +0300

    added RoutesPath to NotFoundPage

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Link } from '@mui/material';
+import { RoutesPath } from '../Router/routesPath';
 import './NotFoundPage.scss';
 
 export const NotFoundPage = () => (
@@ -7,7 +8,7 @@ export const NotFoundPage = () => (
     <h1 className="notfound-page-header">Page not found</h1>
     <p className="notfound-page-404">404</p>
     <p className="notfound-page-info">Let&apos;s use another try.</p>
-    <Link className="notfound-page-button" href="/">
+    <Link className="notfound-page-button" href={RoutesPath.HOME}>
       Home page
     </Link>
   </Container>
