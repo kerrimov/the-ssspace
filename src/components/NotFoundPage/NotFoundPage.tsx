37abc6e5b793ce54d37b446531a9eec0e1b2a0e4
Aleksandra Shevchenko <aleksandra05sh@gmail.com>
2022-05-19 20:11:22 +0300

    added container to notfound page

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link } from '@mui/material';
+import { Container, Link } from '@mui/material';
 import './NotFoundPage.scss';
 
 export const NotFoundPage = () => (
-  <section className="notfound-page">
+  <Container maxWidth="xl" className="notfound-page">
     <h1 className="notfound-page-header">Page not found</h1>
     <p className="notfound-page-404">404</p>
     <p className="notfound-page-info">Let&apos;s use another try.</p>
     <Link className="notfound-page-button" href="/">
       Home page
     </Link>
-  </section>
+  </Container>
 );
-
-export default NotFoundPage;
