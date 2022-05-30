5cd0b94e28b8bef97f1d09c3e146545498d99d83
Volodymyr Klochan <vovkis13@gmail.com>
2022-05-31 00:11:48 +0300

    remove template redux files

--- src/store/app/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-import { StoreState } from '..';
-import type { AppState } from '../types';
-
-export const selectApp = (): AppState => {
-  return useSelector<StoreState, AppState>(state => state.app);
-};
